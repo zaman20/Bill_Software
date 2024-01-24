e062a5eccbee6dfffc2a22ec90f9c39e91a4f230
zaman20 <adorzaman18@gmail.com>
2024-01-24 23:34:55 +0600

    Menu active show

--- View/dashboard.php
+++ View/dashboard.php
@@ -8,7 +8,20 @@
 </head>
 <body>
     <div class="main-container">
-       <?php include('menu.php');?>
+    <div class="menu">
+        <ul>
+            <li><a title="New Buyer" href="newbuyer.php"><img src="../img/new buyer.png" alt=""></a></li>
+            <li><a title="Update Buyer" href="updatebuyer.php"><img src="../img/Update buyer.png" alt=""></a></li>
+            <li><a title="Buyer Details" href="buyerdetails.php"><img src="../img/buyer Details.png" alt=""></a></li>
+            <li><a title="Delete Buyer" href="deletebuyer.php"><img src="../img/delete buyer.png" alt=""></a></li>
+            <li><a title="New Product" href="newproduct.php"><img src="../img/new product.png" alt=""></a></li>
+            <li><a title="Update Product" href="updateproduct.php"><img src="../img/update product.png" alt=""></a></li>
+            <li><a title="Details Product" href="productdetails.php"><img src="../img/details product.png" alt=""></a></li>
+            <li><a title="Delete Product" href="deleteproduct.php"><img src="../img/delete product.png" alt=""></a></li>
+            <li><a title="Bill Making" href="billmaking.php"><img src="../img/billing.png" alt=""></a></li>
+            <li><a title="Log Out" href="../index.php"><img src="../img/logout.png" alt=""></a></li>
+        </ul>
+    </div>
     </div>
 </body>
 </html>
