53a239af643ed7a80b8d72b3bfd598127675dd8e
zaman20 <adorzaman18@gmail.com>
2024-01-30 00:40:58 +0600

    index page design changed

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
     <div class="container">
         <div class="login-box">
+            <h2 style="text-align:center;font-size: 33px; margin-top: -15px;">Billing Preparing System</h2>
             <form action="model/login.php" method="post">
                 <label for="">User Name: </label>
                 <input type="text" name="uname" id="">
